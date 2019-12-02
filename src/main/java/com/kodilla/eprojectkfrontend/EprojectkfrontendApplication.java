c83252d47d4919affd67007871c9368dfa4934c1
ganzes <charczuk.arkadiusz@gmail.com>
2019-12-02 19:28:42 +0100

    initial commit

---
+++ src/main/java/com/kodilla/eprojectkfrontend/EprojectkfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.eprojectkfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprojectkfrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EprojectkfrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/eprojectkfrontend/EprojectkfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.eprojectkfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprojectkfrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
