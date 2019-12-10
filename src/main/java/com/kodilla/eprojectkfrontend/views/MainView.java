9e5926b498de6264335b9ab6856a4d4fcfea9ad5
ganzes <charczuk.arkadiusz@gmail.com>
2019-12-10 01:10:52 +0100

    added vaadin icon size

--- src/main/java/com/kodilla/eprojectkfrontend/views/MainView.java
+++ src/main/java/com/kodilla/eprojectkfrontend/views/MainView.java
@@ -3,6 +3,8 @@ package com.kodilla.eprojectkfrontend.views;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -13,6 +15,8 @@ public class MainView extends VerticalLayout {
         private Button goToLoveView = new Button("Go to Love Calculator!");
         private Button goToQuoteView = new Button("Go to 150000+ Quotes!");
         private Label mainViewLabel = new Label("Welcome to InspirationVibe");
+        private Icon vaadinIcon = new Icon(VaadinIcon.VAADIN_V);
+
 
         //private VerticalLayout verticalLayout = new VerticalLayout();
 
@@ -36,8 +40,12 @@ public class MainView extends VerticalLayout {
 
             mainViewLabel.setWidthFull();
 
+            vaadinIcon.setColor("blue");
+            vaadinIcon.setSize("200px");
+
             add(mainViewLabel);
             add(goToMotiveView, goToLoveView, goToQuoteView);
+            add(vaadinIcon);
 
             setSizeFull();
     }
