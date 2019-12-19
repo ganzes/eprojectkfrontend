549eb7838f2751fb0c33fcb6fff5ca6afa5bcfdc
ganzes <charczuk.arkadiusz@gmail.com>
2019-12-19 03:17:17 +0100

    cleanUp in QuotesService

--- src/main/java/com/kodilla/eprojectkfrontend/services/QuotesService.java
+++ src/main/java/com/kodilla/eprojectkfrontend/services/QuotesService.java
@@ -1,6 +1,8 @@
 package com.kodilla.eprojectkfrontend.services;
 
 import com.kodilla.eprojectkfrontend.domains.QuotesDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -11,28 +13,43 @@ import java.net.URI;
 @Service
 public class QuotesService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuotesService.class);
+
+    private static final QuotesDto errorMessageGetQuoteByAuthorClient =
+            new QuotesDto("Please try again\n", "Example: John, Adam", "", "", "", "", "");
+
+    private static final QuotesDto errorMessageGetQuoteByKeywordClient =
+            new QuotesDto("Please try again\n", "Check you spelling", "Example: Wisdom, Life", "", "", "", "");
+
     private RestTemplate restTemplate = new RestTemplate();
 
     public QuotesDto getQuoteByKeyword(final QuotesDto quotesDto) throws HttpServerErrorException {
-        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/eprojectk/quotes/?keyword=" + quotesDto.getKeywords())
-                .build().encode().toUri();
+        try {
+            URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/eprojectk/quotes/?keyword=" + quotesDto.getKeywords())
+                    .build().encode().toUri();
 
-        QuotesDto quotesDto1 = restTemplate.getForObject(uri, QuotesDto.class);
-        return quotesDto1;
+            return restTemplate.getForObject(uri, QuotesDto.class);
+        } catch (HttpServerErrorException e) {
+            LOGGER.warn("User did not follow tutorial " + e);
+        }
+        return errorMessageGetQuoteByKeywordClient;
     }
 
     public QuotesDto getQuoteByAuthor(final QuotesDto quotesDto) throws HttpServerErrorException {
-        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/eprojectk/quotes/byAuthor?author=" + quotesDto.getAuthor())
-                .build().encode().toUri();
+        try {
+            URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/eprojectk/quotes/byAuthor?author=" + quotesDto.getAuthor())
+                    .build().encode().toUri();
 
-        QuotesDto quotesDto1 = restTemplate.getForObject(uri, QuotesDto.class);
-        return quotesDto1;
+            return restTemplate.getForObject(uri, QuotesDto.class);
+        } catch (HttpServerErrorException e) {
+            LOGGER.warn("User did not follow tutorial " + e);
+        }
+        return errorMessageGetQuoteByAuthorClient;
     }
 
     public QuotesDto getRandomQuote() throws HttpServerErrorException {
         URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/eprojectk/quotes/getRandom")
                 .build().encode().toUri();
-        QuotesDto quotesDto1 = restTemplate.getForObject(uri, QuotesDto.class);
-        return quotesDto1;
+        return restTemplate.getForObject(uri, QuotesDto.class);
     }
 }
